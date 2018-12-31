f90b35f26ad5fb74f64b7f883675023343d7bdda
Mickaël Poussevin <mickael.poussevin@gmail.com>
2018-12-31 13:51:35 +0100

    Remove ^ and ^ from patterns

--- webradio/urls.py
+++ webradio/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'^$', views.index, name='home'),
-    path(r'^stations/$', views.stations, name='webradio_stations'),
-    path(r'^player/$', views.player, name='webradio_player'),
-    path(r'^controls/play/(\d+)/$', views.control_play_station),
-    path(r'^controls/playpause/$', views.control_play_pause),
-    path(r'^controls/nextstation/$', views.control_station_next),
-    path(r'^controls/previousstation/$', views.control_station_previous),
-    path(r'^controls/info/$', views.control_get_info),
+    path('', views.index, name='home'),
+    path('stations/', views.stations, name='webradio_stations'),
+    path('player/', views.player, name='webradio_player'),
+    path('controls/play/(\d+)/', views.control_play_station),
+    path('controls/playpause/', views.control_play_pause),
+    path('controls/nextstation/', views.control_station_next),
+    path('controls/previousstation/', views.control_station_previous),
+    path('controls/info/', views.control_get_info),
 ]
