3e02b62553e48c58dbe41d15b78f6fe4eccedbda
Mickaël Poussevin <mickael.poussevin@gmail.com>
2018-12-31 14:04:59 +0100

    Using namespace for reverse

--- webradio/urls.py
+++ webradio/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from . import views
 
+app_name = 'webradio'
+
 urlpatterns = [
     path('', views.index, name='home'),
-    path('stations/', views.stations, name='webradio_stations'),
-    path('player/', views.player, name='webradio_player'),
-    path('controls/play/(\d+)/', views.control_play_station),
-    path('controls/playpause/', views.control_play_pause),
-    path('controls/nextstation/', views.control_station_next),
-    path('controls/previousstation/', views.control_station_previous),
-    path('controls/info/', views.control_get_info),
+    path('stations/', views.stations, name='stations'),
+    path('player/', views.player, name='player'),
+    path('controls/play/(\d+)/', views.control_play_station,
+         name='control_play_station'),
+    path('controls/playpause/', views.control_play_pause,
+         name='control_play_pause'),
+    path('controls/nextstation/', views.control_station_next,
+         name='control_station_next'),
+    path('controls/previousstation/', views.control_station_previous,
+         name='control_station_previous'),
+    path('controls/info/', views.control_get_info, name='control_get_info'),
 ]
