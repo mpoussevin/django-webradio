947de70ba90b5460b1206b7c9981e79c19e6ed82
Mickaël Poussevin <mickael.poussevin@gmail.com>
2018-12-31 11:56:49 +0100

    Add fields to Meta

--- webradio/forms.py
+++ webradio/forms.py
@@ -1,21 +1,24 @@
 from django import forms
 from webradio.models import Stream, Station
 
+
 class StationForm(forms.ModelForm):
-    default_stream = forms.ModelChoiceField(queryset=Stream.objects.none(), required=False)
+    default_stream = forms.ModelChoiceField(
+        queryset=Stream.objects.none(), required=False)
 
     class Meta:
         model = Station
+        fields = "__all__"
 
     def __init__(self, *args, **kwargs):
         super(StationForm, self).__init__(*args, **kwargs)
         if self.instance.pk:
-            self.fields['default_stream'].queryset = Stream.objects.filter(station=self.instance)
+            self.fields['default_stream'].queryset = Stream.objects.filter(
+                station=self.instance)
             self.fields['default_stream'].required = True
             default_stream = self.instance.get_default_stream()
             if default_stream:
                 self.fields['default_stream'].initial = default_stream.id
-
 
     def save(self, *args, **kwargs):
         if self.instance.pk:
@@ -23,4 +26,3 @@ class StationForm(forms.ModelForm):
             stream.default = True
             stream.save()
         return super(StationForm, self).save(*args, **kwargs)
-
