f0b385f5b94a81b07d183baeb40fbd8d68186980
Mickaël Poussevin <mickael.poussevin@gmail.com>
2019-01-07 16:15:32 +0100

    NoArgsCommand to BaseCommand

--- webradio/management/commands/reloadstreams.py
+++ webradio/management/commands/reloadstreams.py
@@ -1,12 +1,15 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from webradio.models import Station
 
-class Command(NoArgsCommand):
+
+class Command(BaseCommand):
     help = "Reloads all streams from all stations."
-    
-    def handle_noargs(self, **options):
+
+    def handle(self, *args, **options):
         verbosity = int(options.get('verbosity', 1))
         for my_station in Station.objects.all():
             if verbosity > 0:
-                self.stdout.write("Refreshing streams of station \"%s\"." %my_station)
+                self.stdout.write(
+                    "Refreshing streams of station \"%s\"." %
+                    my_station)
             my_station.refresh_streams()
